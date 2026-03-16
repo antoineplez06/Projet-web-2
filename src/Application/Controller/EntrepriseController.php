6f7823a6577b77a1668cc64193f4e486b2e733e8
Gauthier DESCHARS <gauthier.deschars@viacesi.fr>
2026-03-16 09:46:33 +0100

    Tentative de docker

--- src/Application/Controller/EntrepriseController.php
+++ src/Application/Controller/EntrepriseController.php
@@ -9,27 +9,73 @@ use Slim\Views\Twig;
 
 class EntrepriseController
 {
-    
+
     public function liste(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
-       $view = Twig::fromRequest($request);
-    
+        $view = Twig::fromRequest($request);
+
         $entreprises = [
-            ['id' => 1, 'nom' => 'CESI', 'secteur' => 'IA', 'statut' => 'Actif'],
-            ['id' => 2, 'nom' => 'GreenLeaf', 'secteur' => 'Écologie', 'statut' => 'En attente'],
-            ['id' => 3, 'nom' => 'CyberShield', 'secteur' => 'Sécurité', 'statut' => 'Actif'],
-            ['id' => 4, 'nom' => 'BlueHorizon', 'secteur' => 'Logistique', 'statut' => 'Inactif'],
+            [
+                "id" => 1,
+                "nom" => "TechBaguette",
+                "secteur" => "Paris"
+            ],
+            [
+                "id" => 2,
+                "nom" => "ÉcoLogis",
+                "secteur" => "Lyon"
+            ],
+            [
+                "id" => 3,
+                "nom" => "SantéPlus Solutions",
+                "secteur" => "Marseille"
+            ],
+            [
+                "id" => 4,
+                "nom" => "Transport Express Sud",
+                "secteur" => "Toulouse"
+            ],
+            [
+                "id" => 5,
+                "nom" => "Brasserie des Flandres",
+                "secteur" => "Lille"
+            ],
+            [
+                "id" => 6,
+                "nom" => "Horizon Digital",
+                "secteur" => "Bordeaux"
+            ],
+            [
+                "id" => 7,
+                "nom" => "Ateliers Mécaniques Bretons",
+                "secteur" => "Nantes"
+            ],
+            [
+                "id" => 8,
+                "nom" => "Alpes Innovation",
+                "secteur" => "Grenoble"
+            ],
+            [
+                "id" => 9,
+                "nom" => "StartUp Vision",
+                "secteur" => "Strasbourg"
+            ],
+            [
+                "id" => 10,
+                "nom" => "Cosmétiques Azur",
+                "secteur" => "Nice"
+            ]
         ];
 
-        if(isset($args['page'])){
-            $page = (int)$args['page'];
+        if (isset($args['page'])) {
+            $page = (int) $args['page'];
             $perPage = 2;
             $offset = ($page - 1) * $perPage;
             $entreprises = array_slice($entreprises, $offset, $perPage);
         }
 
         return $view->render($response, 'liste-entreprises.html.twig', [
-           'entreprises' => $entreprises
+            'entreprises' => $entreprises
         ]);
     }
 
@@ -37,13 +83,13 @@ class EntrepriseController
     public function ajoute(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
         $view = Twig::fromRequest($request);
-    
+
         $parsedBody = $request->getParsedBody();
         //var_dump($parsedBody); 
 
         //Vérifier l'id
         $success = false;
-        if($request->getMethod() === 'POST'){
+        if ($request->getMethod() === 'POST') {
             //Ici, on ajouterait l'entreprise à la base de données
             $success = true;
         }
@@ -54,13 +100,13 @@ class EntrepriseController
         ]);
     }
 
-    
+
     public function edit(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
-       $view = Twig::fromRequest($request);
+        $view = Twig::fromRequest($request);
 
         return $view->render($response, 'entreprise_formulaire.twig', [
-        
+
         ]);
     }
 }
