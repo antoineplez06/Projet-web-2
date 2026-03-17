59c40b362c03470518beabc3994f672abaa1835e
Gauthier DESCHARS <gauthier.deschars@viacesi.fr>
2026-03-17 16:40:52 +0100

    Workshop

--- app/dependencies.php
+++ app/dependencies.php
@@ -9,15 +9,21 @@ use Monolog\Logger;
 use Monolog\Processor\UidProcessor;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
+// Imports manquants pour Doctrine
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+use Doctrine\DBAL\DriverManager;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
+        // Définition du Logger
         LoggerInterface::class => function (ContainerInterface $c) {
             $settings = $c->get(SettingsInterface::class);
-
             $loggerSettings = $settings->get('logger');
+            
             $logger = new Logger($loggerSettings['name']);
-
             $processor = new UidProcessor();
             $logger->pushProcessor($processor);
 
@@ -25,6 +31,28 @@ return function (ContainerBuilder $containerBuilder) {
             $logger->pushHandler($handler);
 
             return $logger;
+        },
+
+        
+        EntityManager::class => function (ContainerInterface $c) {
+            $settings = $c->get(SettingsInterface::class);
+
+            // Use the ArrayAdapter or the FilesystemAdapter depending on the value of the 'dev_mode' setting
+            // You can substitute the FilesystemAdapter for any other cache you prefer from the symfony/cache library
+            $cache = $settings['doctrine']['dev_mode'] ?
+                new ArrayAdapter() :
+                new FilesystemAdapter(directory: $settings['doctrine']['cache_dir']);
+
+            $config = ORMSetup::createAttributeMetadataConfiguration(
+                $settings['doctrine']['metadata_dirs'],
+                $settings['doctrine']['dev_mode'],
+                null,
+                $cache
+            );
+
+            $connection = DriverManager::getConnection($settings['doctrine']['connection']);
+
+            return new EntityManager($connection, $config);
         },
     ]);
 };
