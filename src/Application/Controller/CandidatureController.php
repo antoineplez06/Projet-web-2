1e5e170dd47532ad1a74b145c3f0e731092911ab
Gauthier DESCHARS <gauthier.deschars@viacesi.fr>
2026-03-31 20:07:26 +0200

    correction postuler et pilote peut voir candidature de ses élèves

--- src/Application/Controller/CandidatureController.php
+++ src/Application/Controller/CandidatureController.php
@@ -13,25 +13,52 @@ class CandidatureController
 {
     private EntityManager $entityManager;
 
-    public function __construct(EntityManager $entityManager) {
+    public function __construct(EntityManager $entityManager)
+    {
         $this->entityManager = $entityManager;
     }
 
-    public function list(Request $request, Response $response): Response {
+    public function list(Request $request, Response $response): Response
+    {
         $view = Twig::fromRequest($request);
-        $candidatures = $this->entityManager->getRepository(Candidature::class)->findAll();
+        $userConnecte = $request->getAttribute('user');
+
+        if (!$userConnecte) {
+            return $response->withHeader('Location', '/connexion')->withStatus(302);
+        }
+
+        $role = strtolower($userConnecte->getRoleValue());
+
+
+        $queryBuilder = $this->entityManager->getRepository(Candidature::class)
+            ->createQueryBuilder('c')
+            ->join('c.etudiant', 'e'); 
+        if ($role === 'etudiant') {
+            $queryBuilder->where('c.etudiant = :etudiant')->setParameter('etudiant', $userConnecte);
+        } elseif ($role === 'pilote') {
+            $campusPilote = $userConnecte->getCampus();
+            if ($campusPilote) {
+                $queryBuilder->where('e.campus = :campus')
+                    ->setParameter('campus', $campusPilote);
+            }
+        }
+
+        $candidatures = $queryBuilder->getQuery()->getResult();
+
         return $view->render($response, 'candidature/liste.html.twig', [
-            'candidatures' => $candidatures
+            'candidatures' => $candidatures,
+            'userRole' => $role 
         ]);
     }
 
-    public function add(Request $request, Response $response, array $args): Response {
+    public function add(Request $request, Response $response, array $args): Response
+    {
         $idOffre = (int)$args['id'];
-        
+        $userConnecte = $request->getAttribute('user');
         $offre = $this->entityManager->getRepository(Offre::class)->find($idOffre);
 
-        if ($offre) {
-            $candidature = new Candidature($offre);
+        if ($offre && $userConnecte) {
+            $candidature = new Candidature($offre, $userConnecte);
             $this->entityManager->persist($candidature);
             $this->entityManager->flush();
         }
--- src/Application/Domain/Candidature.php
+++ src/Application/Domain/Candidature.php
@@ -5,6 +5,7 @@ namespace App\Application\Domain;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
+use App\Application\Domain\user;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\Table;
 use Doctrine\ORM\Mapping\ManyToOne;
@@ -23,12 +24,31 @@ class Candidature
     #[JoinColumn(name: 'idOffre', referencedColumnName: 'idOffre', nullable: false)]
     private Offre $offre;
 
-    public function __construct(Offre $offre)
+    #[ManyToOne(targetEntity: user::class)]
+    #[JoinColumn(name: 'id_etudiant', referencedColumnName: 'id', nullable: false)]
+    private user $etudiant;
+
+    public function __construct(Offre $offre, user $etudiant)
     {
         $this->offre = $offre;
+        $this->etudiant = $etudiant;
     }
 
-    public function getIdCandidature(): int { return $this->id_candidature; }
-    public function getStatut(): string { return $this->statut; }
-    public function getOffre(): Offre { return $this->offre; }
+    public function getIdCandidature(): int
+    {
+        return $this->id_candidature;
+    }
+    public function getStatut(): string
+    {
+        return $this->statut;
+    }
+    public function getOffre(): Offre
+    {
+        return $this->offre;
+    }
+
+    public function getEtudiant(): user
+    {
+        return $this->etudiant;
+    }
 }
